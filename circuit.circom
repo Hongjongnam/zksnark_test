2b7bca4a196933eae97a27353ad2dfdca0292a3d
Saddam-tech <salokhiddinov0727@gmail.com>
2024-02-06 16:31:40 +0900

    credit proof test is successful

--- circuit.circom
+++ circuit.circom
@@ -1,15 +1,32 @@
+pragma circom 2.0.0;
+
 include "./node_modules/circomlib/circuits/poseidon.circom";
+include "./node_modules/circomlib/circuits/comparators.circom";
+include "./node_modules/circomlib/circuits/gates.circom";
 
-template HashString(n) {
-    signal input str[n];
-    signal output hash;
+template CreditProof() {
+    signal input creditScore;
+    signal input minCreditScore;
+    signal input maxCreditScore;
+    signal input address;
+    signal input hash;
 
-    component hasher = Poseidon(n);
-    for (var i = 0; i < n; i++) {
-        hasher.inputs[i] <== str[i];
-    }
+    // creditScore >= minCreditScore && creditScore <= maxCreditScore
+    component lte = LessThan(252);
+    lte.in[0] <== creditScore;
+    lte.in[1] <== maxCreditScore;
 
-    hash <== hasher.out;
+    component gte = GreaterThan(252);
+    gte.in[0] <== creditScore;
+    gte.in[1] <== minCreditScore;
+
+    lte.out * gte.out === 1;
+
+    component poseidon = Poseidon(2);
+    poseidon.inputs[0] <== address;
+    poseidon.inputs[1] <== creditScore;
+    poseidon.out === hash;
+    
 }
 
-component main = HashString(1);
+component main {public [minCreditScore, maxCreditScore, address, hash]} = CreditProof();
---
+++ creditProof.js
@@ -0,0 +1,58 @@
+const circomlibjs = require("circomlibjs");
+const snarkjs = require("snarkjs");
+const fs = require("fs");
+
+async function poseidonHash(inputs) {
+  const poseidon = await circomlibjs.buildPoseidon();
+  const poseidonHash = poseidon.F.toString(poseidon(inputs));
+  return poseidonHash;
+}
+
+// Create credit proof
+async function createCreditProof(
+  address,
+  creditScore,
+  minCreditScore,
+  maxCreditScore
+) {
+  const hash = await poseidonHash([address, creditScore]);
+  const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+    {
+      creditScore: creditScore,
+      minCreditScore: minCreditScore,
+      maxCreditScore: maxCreditScore,
+      address: address,
+      hash: hash,
+    },
+    "./circuit_js/circuit.wasm",
+    "circuit_final.zkey"
+  );
+  return { proof, publicSignals };
+}
+
+async function verifyAgeProof(proof, publicSignals) {
+  const vKey = JSON.parse(fs.readFileSync("verification_key.json"));
+  return await snarkjs.groth16.verify(vKey, publicSignals, proof);
+}
+
+async function main() {
+  try {
+    const { proof, publicSignals } = await createCreditProof(
+      "0x210706cbd9D26c26c727f4d3007D819390934375",
+      700,
+      600,
+      20000
+    );
+    console.log({ proof, publicSignals });
+
+    const verification = await verifyAgeProof(proof, publicSignals);
+    console.log({ verification });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
